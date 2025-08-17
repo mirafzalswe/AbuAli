bbd1a1ec40b61e407ccbf825e8df45b9082c944d
mirafzalswe <mirafzaldevelop@gmail.com>
2025-08-17 12:44:01 +0500

    done

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn uzbek_code.wsgi --log-file -
--- uzbek_code/settings.py
+++ uzbek_code/settings.py
@@ -143,7 +143,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
@@ -185,7 +184,9 @@ CACHES = {
         'TIMEOUT': 3600, # Cache for 1 hour
     }
 }
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    BASE_DIR / 'static',
 ]
+
+# Static files storage for production
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
