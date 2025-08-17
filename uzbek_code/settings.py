c6efc86887c15cc27d9d0c02df847a33564c20df
mirafzalswe <mirafzaldevelop@gmail.com>
2025-08-17 12:37:28 +0500

    whitenoice is deleted

--- uzbek_code/settings.py
+++ uzbek_code/settings.py
@@ -186,7 +186,6 @@ CACHES = {
     }
 }
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
